35357fe0b79b00d85c38dfa04efc5c9880ce67f3
aliaimviz <im.irfanmumtaz21@gmail.com>
2017-10-03 11:14:29 +0500

    adding shortcodes list

--- wp-content/plugins/homepageshortcodes/homemultipleshortcodes.php
+++ wp-content/plugins/homepageshortcodes/homemultipleshortcodes.php
@@ -67,17 +67,17 @@ function rating_result_title($atts = array(), $content = null, $tag){
     ), $atts);
 		$html ='';
 		$html .='<div class="row">
-					  <div class="col-md-2">      
+					  <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">      
 					    <div class="rating-title">
 					      <p>'.$atts["title"].'</p>
 					    </div>
 					  </div>
-					  <div class="col-md-8">
+					  <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
 					   <div class="rating-hr">
 					     <hr>
 					   </div>
 					  </div>
-					  <div class="col-md-2">
+					  <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 extrarating">
 					    <div class="rating-result">
 					      <span>
 					        <div>'.$atts["rate"].'</div>
